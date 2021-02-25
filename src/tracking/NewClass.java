502c31cc21a3d043cac7300fd027f1e71ade9fb5
Dave <davy.castanet@gmail.com>
2021-02-25 15:58:19 -0500

    gloire

---
+++ src/tracking/NewClass.java
@@ -0,0 +1,14 @@
+package tracking;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		System.out.println("does nothing");
+		System.out.println("does nothing even more");
+		System.out.println("does nothing even even more");
+		System.out.println("does nothing even even more");
+
+
+	}
+
+}
---
+++ src/tracking/TrackMe.java
@@ -0,0 +1,14 @@
+package tracking;
+
+public class TrackMe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello tracker");
+		System.out.println("added new line !");
+
+	}
+
+}
+ 
